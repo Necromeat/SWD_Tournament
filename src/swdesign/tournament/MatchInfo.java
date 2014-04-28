948bddecfd449be3ae0c2a46d67c28420ee4dc75
Necromeat <lars_andrew@hotmail.com>
2014-04-28 09:39:46 +0200

    Test Round 1 ResultList pass

---
+++ src/swdesign/tournament/MatchInfo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package swdesign.tournament;
+
+import swdesign.game.GameInstance.Result;
+
+/**
+ *
+ * @author tog
+ */
+public interface MatchInfo
+{
+    public int matchID();
+    public ParticipantInfo getParticipantA();
+    public ParticipantInfo getParticipantB();
+    public boolean hasFinished();
+    public Result getResult();
+}
